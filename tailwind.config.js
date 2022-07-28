75fbe5079d7ac2b27daa7e9be8c441a0a6d5dd81
Aditya Shelke <adi.cs.night@gmail.com>
2022-07-28 16:09:39 +0530

    Extend: transitionProperty and Varient added for animation of height of Div(Thumbnail-title-div)

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,17 @@ module.exports = {
         'gradient-to-b':
           'linear-gradient(to bottom,rgba(20,20,20,0) 0,rgba(20,20,20,.15) 15%,rgba(20,20,20,.35) 29%,rgba(20,20,20,.58) 44%,#141414 68%,#141414 100%);',
       },
+      transitionProperty: {
+        'height': 'height'
+      },
+      variants : {
+        height: ["responsive", "hover", "focus"]
+      },
     },
   },
+   
   plugins: [
     require('tailwind-scrollbar-hide'),
     require('tailwind-scrollbar'),
-  ]}
+  ]
+}
