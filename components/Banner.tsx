d9c8ac9013862556b6aaefe4004d0550ae65b00a
Aditya Shelke <adi.cs.night@gmail.com>
2022-08-12 22:11:32 +0530

    Popup for the moreInfo

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,6 +5,7 @@ import { Movie } from '../typings'
 import { FaPlay } from "react-icons/fa";
 import { InformationCircleIcon } from '@heroicons/react/solid';
 import MoreInfo from './MoreInfo';
+import Popup from 'reactjs-popup';
 
 interface Props {
   netflixOriginals: Movie[]
@@ -13,8 +14,8 @@ interface Props {
 function Banner({ netflixOriginals }: Props) {
   const [movie, setMovie] = useState<Movie | null>(null)
   const [showMoreInfo, setShowMoreInfo] = useState<boolean>(false)
-  
- 
+
+
   useEffect(() => {
     setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)])
 
@@ -32,29 +33,21 @@ function Banner({ netflixOriginals }: Props) {
       </div>
       <div className="overviewAndButtons relative top-52 space-y-3 ml-2 md:ml-4 md:top-80">
 
-      <h1 className='text-2xl lg:text-5xl md:text-4xl font-bold'>
-        {movie?.title || movie?.name || movie?.original_name}
-      </h1>
-      <p className='max-w-xs text-xs md:max-w-lg md:text-sm lg:max-w-2xl lg:text-base'>
-        {movie?.overview}
-      </p>
+        <h1 className='text-2xl lg:text-5xl md:text-4xl font-bold'>
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <p className='max-w-xs text-xs md:max-w-lg md:text-sm lg:max-w-2xl lg:text-base'>
+          {movie?.overview}
+        </p>
 
 
-      <div className='flex space-x-4 '>
-        <button className='bannerButton bg-white text-black'>
-          <FaPlay className='h-4 w-4 text-black md:h-4 md:w-4' />
-          Play
-        </button>
-
-        <button className='bannerButton bg-[gray]/70' onClick={()=>setShowMoreInfo(true)}>
-          More Info
-          <div className={`${!showMoreInfo && "hidden"}`}>
-
-          <MoreInfo movie = {movie}/>
-          </div>
-          <InformationCircleIcon className='h-5 w-5 md:h-8 md:w-8' />
-        </button>
-      </div>
+        <div className='flex space-x-4 '>
+          <button className='bannerButton bg-white text-black'>
+            <FaPlay className='h-4 w-4 text-black md:h-4 md:w-4 ' />
+            Play
+          </button>
+          <MoreInfo movie = {movie} />
+        </div>
 
       </div>
     </div>
