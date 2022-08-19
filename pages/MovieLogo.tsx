f38015e937d15ac01ddf7d49e4f2c9ebd72b40ad
Aditya Shelke <adi.cs.night@gmail.com>
2022-08-19 21:10:16 +0530

    fetched the backdrop in movieLogo.tsx

--- pages/MovieLogo.tsx
+++ pages/MovieLogo.tsx
@@ -1,20 +1,47 @@
-import React from 'react'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+import { baseUrl } from '../constants/movie'
 import { Movie } from '../typings'
 import requests from '../utils/requests'
 
 interface Props{
   movie: Movie | null
+
 }
-
-
 
 function MovieLogo( {movie}: Props ) {
+  const [data, setData] = useState<Movie|null>(null)
 
-  console.log(movie)
+  
+  
+  
+  
+  //To get send l   ogo request to api of the required movie (Client side rendering)
+  
+  useEffect( () => {
+
+    fetch(`https://api.themoviedb.org/3/tv/${movie?.id}?api_key=${process.env.NEXT_PUBLIC_API_KEY}`)
+    .then((res) => res.json())
+    .then((data) => {
+      setData(data)
+    }) 
+    return
+  
+  }, [movie?.id])   
+  console.log(data)
+
+  
+
 
   return (
-    <div>MovieLogo</div>
+    <div>
+      <Image src={`https://image.tmdb.org/t/p/original/${data?.backdrop_path}`}
+      height={"20px"}
+      width={"40px"} />
+    </div>
   )
 }
+
+
 
 export default MovieLogo
